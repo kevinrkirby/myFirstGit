6004ea19bcfd8bd2c13b05d02e1c28085d99b2ea
techn3rd <{6035483}+{kevinrkirby}@users.noreply.github.com>
2018-07-14 14:16:27 +1000

    project ready

---
+++ express1.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+// Log the requests
+app.use(logger('dev'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '/'))); 
+
+// Route for everything else.
+app.get('*', function(req, res){
+  res.send('Hello World');
+});
+
+// Fire it up!
+app.listen(8080);
+console.log('Listening on port 8080');
