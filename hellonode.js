eb71340233f24b4ce53069cb78da450092dfed8a
techn3rd <6035483+kevinrkirby@users.noreply.github.com>
2018-07-14 14:35:45 +1000

    Update hellonode.js

--- hellonode.js
+++ hellonode.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var http = require("http");
 var fs = require('fs');
 var port = 8080;
@@ -25,26 +24,7 @@ var server = http.createServer(function (req, res) {
 
 });
 
-console.log("Starting web server at " + serverUrl + ":" + port);
-server.listen(port, serverUrl);
-=======
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
-
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-//listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
   console.log("hello world");  
   console.log(`Server running at http://${hostname}:${port}/`);
 });
->>>>>>> 771c62e9f43d008d5dc78a8fa8927e4faf27b23d
