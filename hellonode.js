c8c08825b049599056994594688d014d0549df34
techn3rd <{6035483}+{kevinrkirby}@users.noreply.github.com>
2018-07-15 14:49:01 +1000

    setup page not found 'not-found.html

--- hellonode.js
+++ hellonode.js
@@ -11,9 +11,9 @@ app.use(express.static(path.join(__dirname, '/')));
 
 // Route for everything else.
 app.get('*', function(req, res){
-  res.send('Hello World');
-});
+    res.sendFile(path.join(__dirname + '/not-found.html'));
+  });
 
 // Fire it up!
 app.listen(8080);
-console.log('Listening http://127.0.0.1:8080/');
+console.log('Listening here http://127.0.0.1:8080/');
