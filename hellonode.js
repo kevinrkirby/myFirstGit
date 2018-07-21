fddd9a76957c57e5796ebfa7d48d5dd9bcf59481
techn3rd <{6035483}+{kevinrkirby}@users.noreply.github.com>
2018-07-22 04:45:55 +1000

    modifed server setup, added www/ folder for html and css and created a empty login.html file and setup routing

--- hellonode.js
+++ hellonode.js
@@ -1,19 +1,25 @@
-var path = require('path');
 var express = require('express');
-var logger = require('morgan');
 var app = express();
-
-// Log the requests
-app.use(logger('dev'));
+var http = require('http').Server(app);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '/'))); 
+app.use(express.static(__dirname + '/www/')); 
+
+// Route for login.
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + '/www/login.html');
+});
 
 // Route for everything else.
 app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/not-found.html'));
+    res.sendFile(__dirname + '/www/not-found.html');
   });
 
-// Fire it up!
-app.listen(8080);
-console.log('Listening here http://127.0.0.1:8080/');
+
+
+var server= http.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("My First Nodejs Server!");
+  console.log("Server listening on: "+ host + " port:" + port);
+});
