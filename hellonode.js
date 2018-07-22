de89d98696880cda65b146d85270d2d3800833fd
techn3rd <{6035483}+{kevinrkirby}@users.noreply.github.com>
2018-07-22 13:23:19 +1000

    modifed server setup, added auth/ folder for login and created a login form. about to setup post to attempt.js

--- hellonode.js
+++ hellonode.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var bodyParser = require('body-parser');
 
 // Serve static files
 app.use(express.static(__dirname + '/www/')); 
 
 // Route for login.
 app.get('/login', function(req, res){
-  res.sendFile(__dirname + '/www/login.html');
+  res.sendFile(__dirname + '/www/auth/login.html');
 });
 
+app.get('/attempt.js', function(req, res) {
+  res.sendFile(__dirname + '/www/auth/attempt.js');
+});
 // Route for everything else.
 app.get('*', function(req, res){
     res.sendFile(__dirname + '/www/not-found.html');
@@ -17,7 +21,7 @@ app.get('*', function(req, res){
 
 
 
-var server= http.listen(3000, function () {
+var server = http.listen(3000, "127.0.0.1", function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("My First Nodejs Server!");
